8c0a50b0b9ad596e153a821b6c2d25d5bc489279
JustlfC <98635147+JustlfC03@users.noreply.github.com>
2023-11-25 23:39:02 +0800

    Add files via upload

---
+++ exp.voc/voc8.res50v3+.CPS+DFCPS/train.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+nvidia-smi
+
+export volna="path to your data dir"
+export NGPUS=8
+export OUTPUT_PATH="/home/lmz/lmz/TorchSemiSeg-main/TorchSemiSeg-main/exp.voc/voc8.res50v3+.CPS/results"
+export snapshot_dir=$OUTPUT_PATH/snapshot
+
+export batch_size=8
+export learning_rate=0.0025
+export snapshot_iter=1
+
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --nproc_per_node=$NGPUS train.py
+export TARGET_DEVICE=$[$NGPUS-1]
+#python eval.py -e 20-34 -d 0-$TARGET_DEVICE --save_path $OUTPUT_PATH/results
+
+# following is the command for debug
+# export NGPUS=1
+# export batch_size=2
+# python -m torch.distributed.launch --nproc_per_node=$NGPUS train.py --debug 1
